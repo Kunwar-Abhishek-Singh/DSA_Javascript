b2eb3aa177f9c3f4bafd8c065c54335950eb1ecc
Kunwar-Abhishek-Singh <aksgaya6@gmail.com>
2025-05-04 18:53:06 +0530

    array traversing method commited

---
+++ Array/traversing.js
@@ -0,0 +1,6 @@
+//so basically traversing is nothing but the reading of values from an array is known as traversing of an array.
+
+
+// if i have to read the value of the 4th element from the array 
+let array = [23, 33, 34, 21, 46]
+console.log("4th element of the array is: ", array[3]);
